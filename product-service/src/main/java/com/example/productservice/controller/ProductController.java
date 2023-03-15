37ec5bc357a49334d1f316d4218f3fe3904480c6
fortune <fortuneojih@yahoo.com>
2023-03-15 22:33:54 +0100

    product request dto

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.productservice.controller;
 
+import com.example.productservice.dto.ProductRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
