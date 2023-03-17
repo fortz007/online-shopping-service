33d6208dc633b1356ba6c53bba579a8ccae79e49
fortune <fortuneojih@yahoo.com>
2023-03-17 11:09:11 +0100

    modified request controller to check inStock status

--- inventory-service/src/main/java/com/fortuneprogramming/inventoryservice/controllers/InventoryController.java
+++ inventory-service/src/main/java/com/fortuneprogramming/inventoryservice/controllers/InventoryController.java
@@ -1,17 +1,21 @@
 package com.fortuneprogramming.inventoryservice.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fortuneprogramming.inventoryservice.services.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
 
+    private final InventoryService inventoryService;
     @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@PathVariable("sku-code") String skuCode){
-        return
+        boolean inStock = inventoryService.isInStock(skuCode);
+        return inStock;
     }
 
 }
